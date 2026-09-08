d7f5fddebc24c3f9748e06c1d09f568f3179fd38
TeamIAlchemist <frjnnvmmth@privaterelay.appleid.com>
2026-09-08 10:58:55 +0200

    Create next.config.js

---
+++ allIAnce-app/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // Ignore les erreurs TypeScript pendant le build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignore les avertissements ESLint pendant le build
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
